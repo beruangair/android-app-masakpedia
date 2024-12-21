e534eee075bdef41a6216f0e57a4ff15e2de0f9a
Arif Lutfhansah <aplikasi.arif@gmail.com>
2024-12-21 11:48:02 +0700

    Tambah Splashscreen

---
+++ app/src/main/java/com/ariflutfhansah/masakpedia/SplashActivity.kt
@@ -0,0 +1,41 @@
+package com.ariflutfhansah.masakpedia
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.caverock.androidsvg.SVG
+import com.caverock.androidsvg.SVGImageView
+import java.io.InputStream
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        // Mengatur padding untuk menghindari konten tertutup oleh sistem bar
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Mengatur SVG ke SVGImageView
+        val svgImageView: SVGImageView = findViewById(R.id.app_logo)
+        val inputStream: InputStream = resources.openRawResource(R.raw.app_icon)
+        val svg: SVG = SVG.getFromInputStream(inputStream)
+        svgImageView.setSVG(svg)
+
+        // Menampilkan splash screen selama 3 detik
+        Handler().postDelayed({
+            // Pindah ke MainActivity setelah splash screen
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Menutup SplashActivity
+        }, 3000) // 3000 ms = 3 detik
+    }
+}
